344e0aa1297e066299af21776fb9a4ee9ab556bb
Ingrid G <aleja.morada@gmail.com>
2019-04-02 19:09:59 -0700

    its all i got

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.json());
+
+//configuration fo connection
 //importing the sqlite module
 const sqlite3 = require('sqlite3').verbose();
+// const sqlite3 = new sqlite3({
+//     user: 'me',
+//     host: 'localhost',
+//     database: 
+// })
 
-const PORT = 3000
+
+let db = new sqlite3.Database('/Users/tpl3/permits.db', sqlite3.OPEN_READONLY, (err) => {
+    if(err) {
+        console.log(err.message);
+    }
+    console.log('Connected to the permits database.');
+});
+
+db.serialize(() => {
+    db.each(`SELECT `)
+})
+
+//To Close database
+function closeDb() {
+    console.log("closeDb");
+    db.close();
+}
 
 //testing 
 app.get('/', function(request, respond) {
     respond.send('Bom dia mundo')
+    console.log('it worked!')
 })
 
-app.listen(PORT, () => console.log(`tessssting on port ${PORT}`))
+app.listen(PORT, () => console.log(`testing, you are LIVE  - on port ${PORT}!!!`))
+
